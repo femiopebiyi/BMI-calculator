2566d73677b5bbf72c54d835bdebe6f13250aa28
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-24 22:36:24 +0100

    added us metric

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const heightInput  = document.querySelector('#height');
 const weightInput  = document.querySelector('#weight');
 const resultDisplay = document.querySelector('p');
-const computeBMI  = document.querySelector('button');
+const computeBMI  = document.querySelector('.compute');
 let info = document.querySelectorAll('li')
+const usUnits = document.querySelector("#us-units")
+const metricUnits = document.querySelector("#metric-units")
 
 function checkIfObese (BMI){
     if(BMI<= 18.5){
@@ -52,7 +54,8 @@ function calculateBMI (){
 }
 
 computeBMI.addEventListener('click', function(){
-    if(isNaN(heightInput.value * 5) || isNaN(weightInput.value * 5)){
+    if(weightInput.placeholder === 'kilograms'){
+        if(isNaN(heightInput.value * 5) || isNaN(weightInput.value * 5)){
         resultDisplay.innerHTML = 'Please input value in numbers only!!'
         setTimeout(function(){
             resultDisplay.innerHTML = `Your BMI is:`
@@ -65,6 +68,22 @@ computeBMI.addEventListener('click', function(){
     } else{
         calculateBMI()
     }
+    } else{
+        if(isNaN(heightInput.value * 5) || isNaN(weightInput.value * 5)){
+        resultDisplay.innerHTML = 'Please input value in numbers only!!'
+        setTimeout(function(){
+            resultDisplay.innerHTML = `Your BMI is:`
+        }, 2000)
+    }else if(heightInput.value === '' || weightInput.value === ''){
+        resultDisplay.innerHTML = `don't leave any input empty!!`
+        setTimeout(function(){
+            resultDisplay.innerHTML = `Your BMI is:`
+        }, 2000)
+    } else{
+        calculateBMIUS()
+    }
+    }
+    
     
     
     
@@ -91,16 +110,82 @@ document.body.addEventListener('keypress', function(event){
 
 
 
+const labels = document.querySelectorAll('label')
+
+
+metricUnits.classList.add('clicked')
+metricUnits.addEventListener ('click', function(){
+    usUnits.classList.remove('clicked');
+    metricUnits.classList.add('clicked');
+    labels[0].innerHTML = 'Your Height(cm)';
+    labels[1].innerHTML = 'Your Weight(kg)';
+    heightInput.placeholder = 'centimetres';
+    weightInput.placeholder = 'kilograms';
+    heightInput.value = '';
+    weightInput.value = '';
+    resultDisplay.innerHTML = 'Your BMI:'
+    info.forEach(function(unitInfo){
+        unitInfo.classList.remove('match')
+    })
+})
+
+usUnits.addEventListener ('click', function(){
+    usUnits.classList.add('clicked');
+    metricUnits.classList.remove('clicked');
+    labels[0].innerHTML = 'Your Height(ft)';
+    labels[1].innerHTML = 'Your Weight(lb)';
+    heightInput.placeholder = 'feets';
+    weightInput.placeholder = 'pounds';
+    heightInput.value = ''
+    weightInput.value = ''
+    resultDisplay.innerHTML = 'Your BMI:'
+    info.forEach(function(unitInfo){
+        unitInfo.classList.remove('match')
+    })
+
+})
 
 
 
-// buttons.forEach(function(button){
-//     button.addEventListener('click', function(){
-//         button.classList.add('clicked')
-//         buttons.forEach(function(otherButton){
-//             if(otherButton !== button){
-//                 otherButton.classList.remove('clicked')
-//             }
-//         })
-//     })
-// })
+
+function calculateBMIUS (){
+    let heightNum = Number(heightInput.value);
+    let weightNum = Number(weightInput.value);
+    let heightInInches = heightNum*12
+
+    
+
+    let BMI = ((weightNum/(heightInInches*heightInInches)) * 703 ).toFixed(1)
+
+    checkIfObese(BMI)
+
+
+    if(BMI< 18.5){
+        info[0].classList.add('match')
+        info[1].classList.remove('match')
+        info[2].classList.remove('match')
+        info[3].classList.remove('match')
+    } else if(BMI>=18.5 && BMI<=24.9){
+        info[1].classList.add('match')
+        info[0].classList.remove('match')
+        info[2].classList.remove('match')
+        info[3].classList.remove('match')
+    } else if(BMI>=25 && BMI<=29.9){
+        info[2].classList.add('match')
+        info[1].classList.remove('match')
+        info[3].classList.remove('match')
+        info[0].classList.remove('match')
+    } else if(BMI>=30){
+        info[3].classList.add('match')
+        info[0].classList.remove('match')
+        info[1].classList.remove('match')
+        info[2].classList.remove('match')
+    }
+}
+
+
+computeBMI.addEventListener('click', function(){
+    if(weightInput.placeholder === 'pounds'){
+        calculateBMIUS()
+    }
+})
